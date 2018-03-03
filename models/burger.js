cd9ce7fe6071bf9fe791995fd559ecf1e24bdcce
Phil S <philipstubbs13@gmail.com>
2018-03-03 17:26:16 -0600

    got something displaying to the page. #progress

--- models/burger.js
+++ models/burger.js
@@ -1,9 +1,26 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
 
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
 
-
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+};
 
 // Export the database functions for the controller (burgers_controller.js).
-//module.exports = burger;
+module.exports = burger;
