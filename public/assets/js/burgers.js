deb887153d8c7adca46bef8c7ddd9bb97a7ebcb6
Phil S <philipstubbs13@gmail.com>
2018-03-04 15:10:59 -0600

    got devour button working

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -37,5 +37,28 @@ $(function() {
           }
         );
       });
+
+    $(".change-devour").on("click", function(event) {
+        var id = $(this).data("id");
+        var newDevour = $(this).data("newdevour");
+    
+        var newDevourState = {
+          devoured: "true"
+        };
+    
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+          type: "PUT",
+          data: newDevourState
+        }).then(
+          function() {
+            console.log("changed devour to", newDevour);
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      });
+    
+    
     
 });
